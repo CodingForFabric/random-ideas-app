3cb2bb6e99a19c58340d263b94a6846ac2b0007f
Ethan <baloghethan@gmail.com>
2024-02-09 04:26:13 -0500

    final

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const path = require('path');
 const express = require('express');
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+
 connectDB();
+
 const app = express();
 
-//Satic Folder
+// Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Body parser middleware
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//card middleware
+// cors middleware
 app.use(
   cors({
     origin: ['http://localhost:5000', 'http://localhost:3000'],
@@ -23,12 +25,10 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the randomideas api' });
+  res.json({ message: 'Welcome to the RandomIdeas API' });
 });
 
 const ideasRouter = require('./routes/ideas');
 app.use('/api/ideas', ideasRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
-});
+app.listen(port, () => console.log(`Server listening on port ${port}`));
